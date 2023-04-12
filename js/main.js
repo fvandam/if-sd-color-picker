0a13f27e186dad675ec1deb2482fbcfc595a8082
fvandam <302683815@student.rocmondriaan.nl>
2023-04-12 15:23:03 +0200

    added a and fixed G and B

--- js/main.js
+++ js/main.js
@@ -2,20 +2,21 @@ console.log("main.js loaded");
 
 // Variables
 const bgColor = {
-    r: 12, 
+    r: 12,
     g: 50,
     b: 100,
+    a: 10,
 
     toCSS: function () {
-        let result = `rgb(${this.r},${this.g}, ${this.b})`;
+        let result = `rgb(${this.r},${this.g}, ${this.b}, ${this.a/10})`;
 
         return result;
     },
 
     toHex: function () {
-        let result = "";
-        // bewerk hier het resultaat naar een hexadecimaal getal. 
-        
+        let result = "#";
+    
+
         return result;
     }
 }
@@ -26,15 +27,59 @@ const redSlider = document.querySelector("input.red");
 redSlider.addEventListener("change", (event) => {
     bgColor.r = event.target.value;
     console.log("changing red: " + bgColor.r)
-    
+
     changeCircleColor();
 });
 
+const greenSlider = document.querySelector("input.green");
 
+greenSlider.addEventListener("change", (event1) => {
+    bgColor.g = event1.target.value;
+    console.log("changing green: " + bgColor.g)
+
+    changeCircleColor();
+});
+
+const blueSlider = document.querySelector("input.blue");
+
+blueSlider.addEventListener("change", (event2) => {
+    bgColor.b = event2.target.value;
+    console.log("changing blue: " + bgColor.b)
+
+    changeCircleColor();
+});
+
+const alphaSlider = document.querySelector("input.alpha");
+
+alphaSlider.addEventListener("change", (event3) => {
+    bgColor.a = event3.target.value;
+    console.log("changing alpha: " + bgColor.a)
+
+    changeCircleColor();
+});
 //Functions
-function changeCircleColor(){
+function changeCircleColor() {
     const circleDiv = document.querySelector("div.circle");
-    circleDiv.style.backgroundColor = bgColor.toCSS(); 
+    circleDiv.style.backgroundColor = bgColor.toCSS();
+
+    const rValue = document.querySelector('.r-value');
+    const gValue = document.querySelector('.g-value');
+    const bValue = document.querySelector('.b-value');
+    const aValue = document.querySelector('.a-value')
+
+    rValue.textContent = bgColor.r;
+    gValue.textContent = bgColor.g;
+    bValue.textContent = bgColor.b;
+    aValue.textContent = bgColor.a/10;
+
+    const hexR = Number(bgColor.r).toString(16);
+    const hexG = Number(bgColor.g).toString(16);
+    const hexB = Number(bgColor.b).toString(16);
+    const hexA = Number(bgColor.a).toString(16);
+    console.log(hexR + hexG + hexB + hexA);
+
+    const showHex = document.querySelector('.show-hex');
+    showHex.innerHTML = '#' + hexR + hexG + hexB + hexA;
     
 }
 
